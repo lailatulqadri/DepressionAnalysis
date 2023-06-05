3b8332bda31d1e0b9aef75d9b9d7f4e7fcfcdf35
lailatulqadri <61860775+lailatulqadri@users.noreply.github.com>
2023-06-05 22:36:35 +0800

    Update depression.py

--- depression.py
+++ depression.py
@@ -2,3 +2,14 @@ import streamlit as st
 import pandas as pd
 
 st.title("Extracting Depression Linguistic Features by using Natural Language Processing")
+
+#user may add text for analysis
+
+txt_input = st.text_area('Please add text to analyze', '''
+    Example: It was the best of times, it was the worst of times, it was
+    the age of wisdom, it was the age of foolishness, it was
+    the epoch of belief, it was the epoch of incredulity, it
+    was the season of Light, it was the season of Darkness, it
+    was the spring of hope, it was the winter of despair, (...)
+    ''')
+st.write('Analyse:', run_sentiment_analysis(txt_input))
