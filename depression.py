cac27c65ff519120c6890a4355d2dde60b300617
lailatulqadri <61860775+lailatulqadri@users.noreply.github.com>
2023-06-06 00:02:05 +0800

    Update depression.py

--- depression.py
+++ depression.py
@@ -6,20 +6,19 @@ lexicon = Empath()
 
 
 st.title("Extracting Depression Linguistic Features by using Natural Language Processing")
-container1 = st.container()
 #user may add text for analysis
-container1.text("Let's play with empath library to extract general linguistic features from text")
-txt_input = container1.text_area('Please add text to analyze', ''' Not wanting to do anything. Not wanting to be anything. Not wanting to be at all. I don't necessarily want to die. I just want to have never existed.
+st.text("Let's play with empath library to extract general linguistic features from text")
+txt_input = st.text_area('Please add text to analyze', ''' Not wanting to do anything. Not wanting to be anything. Not wanting to be at all. I don't necessarily want to die. I just want to have never existed.
     ''')
-container1.button("Analyse", on_click = run_empath)
+st.button("Analyse", on_click = run_empath)
 
-container2 = st.container()
+
 def run_empath():
-    container2.write("Empath")
+    st.write("Empath")
     output = lexicon.analyze(txt_input, normalize=True)
     #st.write(output)
     df = pd.DataFrame([output])
-    container2.dataframe(df)
+    st.dataframe(df)
     #st.expender(st.write("testing"))
   
     
