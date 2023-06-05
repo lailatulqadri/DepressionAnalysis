32ed1af3e0e7b6a1461ad9d5e8e993cdcd494f67
lailatulqadri <61860775+lailatulqadri@users.noreply.github.com>
2023-06-05 23:24:11 +0800

    Update depression.py

--- depression.py
+++ depression.py
@@ -8,11 +8,7 @@ st.title("Extracting Depression Linguistic Features by using Natural Language Pr
 #user may add text for analysis
 
 txt_input = st.text_area('Please add text to analyze', '''
-    Example: It was the best of times, it was the worst of times, it was
-    the age of wisdom, it was the age of foolishness, it was
-    the epoch of belief, it was the epoch of incredulity, it
-    was the season of Light, it was the season of Darkness, it
-    was the spring of hope, it was the winter of despair, (...)
+Not wanting to do anything. Not wanting to be anything. Not wanting to be at all. I don't necessarily want to die. I just want to have never existed.
     ''')
 
 def run_empath():
