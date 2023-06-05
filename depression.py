2a6c90ff0a3311ec65b54a2de909afb0bd4499d2
lailatulqadri <61860775+lailatulqadri@users.noreply.github.com>
2023-06-05 23:58:24 +0800

    Update depression.py

--- depression.py
+++ depression.py
@@ -1,22 +1,25 @@
 import streamlit as st
 import pandas as pd
+
 from empath import Empath
 lexicon = Empath()
 
-st.title("Extracting Depression Linguistic Features by using Natural Language Processing")
 
+st.title("Extracting Depression Linguistic Features by using Natural Language Processing")
+container1 = st.container()
 #user may add text for analysis
-st.text("Let's play with empath library to extract general linguistic features from text")
-txt_input = st.text_area('Please add text to analyze', ''' Not wanting to do anything. Not wanting to be anything. Not wanting to be at all. I don't necessarily want to die. I just want to have never existed.
+container1.text("Let's play with empath library to extract general linguistic features from text")
+txt_input = container1.text_area('Please add text to analyze', ''' Not wanting to do anything. Not wanting to be anything. Not wanting to be at all. I don't necessarily want to die. I just want to have never existed.
     ''')
 
+container2 = st.container()
 def run_empath():
-    st.write("Empath")
+    container2.write("Empath")
     output = lexicon.analyze(txt_input, normalize=True)
     #st.write(output)
     df = pd.DataFrame([output])
-    st.dataframe(df)
-    st.expender(st.write("testing"))
+    container2.dataframe(df)
+    #st.expender(st.write("testing"))
   
     
     
