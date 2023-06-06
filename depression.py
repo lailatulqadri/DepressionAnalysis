e0ee967aa4e4fc9800a7ad7663663177e1fbf371
lailatulqadri <61860775+lailatulqadri@users.noreply.github.com>
2023-06-06 22:00:41 +0800

    Update depression.py

--- depression.py
+++ depression.py
@@ -13,22 +13,30 @@ st.text("Let's play with empath library to extract general linguistic features f
 txt_input = st.text_area('Please add text to analyze', ''' Not wanting to do anything. Not wanting to be anything. Not wanting to be at all. I don't necessarily want to die. I just want to have never existed.
     ''')
 
- 
-empath_output = st.button("Analyse Empath")
-emotion_output = st.button("Analyse Emotion")
+col1, col2, col3 = st.columns(3)
 
-if empath_output:
-    st.write("Empath Output")
-    output = lexicon.analyze(txt_input, normalize=True)
-    #st.write(output)
-    df = pd.DataFrame([output])
-    st.dataframe(df.T)
+with col1:
+    empath_output = st.button("Analyse Empath")
+    if empath_output:
+        st.write("Empath Output")
+        output = lexicon.analyze(txt_input, normalize=True)
+        #st.write(output)
+        df = pd.DataFrame([output])
+        st.dataframe(df.T)
 
-if emotion_output:
-    st.write("Emotion Output")
-    emo=LeXmo.LeXmo(txt_input)
-    df_emo = pd.DataFrame([emo])
-    st.dataframe(df_emo.T)
+with col2:
+    emotion_output = st.button("Analyse Emotion")
+    if emotion_output:
+        st.write("Emotion Output")
+        emo=LeXmo.LeXmo(txt_input)
+        df_emo = pd.DataFrame([emo])
+        st.dataframe(df_emo.T)
+
+with col3:
+    st.write("In progress..")
+
+
+
 
 
 
