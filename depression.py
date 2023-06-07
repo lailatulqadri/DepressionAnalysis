a8893090e9862212c5f16bd35a2ca256bad7291d
lailatulqadri <61860775+lailatulqadri@users.noreply.github.com>
2023-06-07 23:27:58 +0800

    Update depression.py

--- depression.py
+++ depression.py
@@ -14,7 +14,7 @@ import contractions
 
 #list - important text features for depression identification:
 # 1) absolute words 
-absolutist_word =['absolutely','all','always','complete','completelt','constant','constantly','definitely','entire','ever','every','everyone','everything','full','must','never','nothing','totally','whole']
+absolutist_word =['absolutely','all','always','complete','completely','constant','constantly','definitely','entire','ever','every','everyone','everything','full','must','never','nothing','totally','whole']
 # 2) first person singular & plurl (sometimes we use plural to refer to ourselve)
 first_person_singular = ['me','myself', 'i', 'mine', 'my', 'ourself']
 first_person_plural = ['we', 'us', 'our', 'ours', 'ourselves']
