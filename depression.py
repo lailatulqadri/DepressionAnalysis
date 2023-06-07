0713bff3b118de42ae5189ebc3c226304558f681
lailatulqadri <61860775+lailatulqadri@users.noreply.github.com>
2023-06-07 22:22:43 +0800

    Update depression.py

--- depression.py
+++ depression.py
@@ -19,9 +19,8 @@ first_person_plural = ['we', 'us', 'our', 'ours', 'ourselves']
 
 st.title("Extracting Depression Linguistic Features by using Natural Language Processing")
 #user may add text for analysis
-#st.write("Let's play with empath library to extract general linguistic features from text")
-#txt_input = st.text_area('Please add text to analyze', ''' Not wanting to do anything. Not wanting to be anything. Not wanting to be at all. I don't necessarily want to die. I just want to have never existed.
-#    ''')
+st.txt("Let's play with empath library to extract general linguistic features from text")
+txt_input = st.text_area('Please add text to analyze', ''' Not wanting to do anything. Not wanting to be anything. Not wanting to be at all. I don't necessarily want to die. I just want to have never existed. ''')
 
 #col1, col2, col3 = st.columns(3)
 
