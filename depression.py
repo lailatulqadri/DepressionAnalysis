6675f1816af79e34e47711969e85d76f71e2e317
lailatulqadri <61860775+lailatulqadri@users.noreply.github.com>
2023-06-07 21:20:44 +0800

    Update depression.py

--- depression.py
+++ depression.py
@@ -55,7 +55,7 @@ with col3:
     #st.write(txt_input1)
     #set to lower case
     list_word = word_tokenize(txt_input1.lower())
-    st.write("number of words", list_word.len())
+    st.write(list_word.len())
     pronoun_first_person = []
     for word in list_word:
         if word in first_person_singular:
