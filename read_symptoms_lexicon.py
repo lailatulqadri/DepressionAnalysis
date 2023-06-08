07cc4c83d44568087f749eebca5caa3137194954
Lailatul Qadri Zakaria <61860775+lailatulqadri@users.noreply.github.com>
2023-06-08 22:29:10 +0800

    Update read_symptoms_lexicon.py

--- read_symptoms_lexicon.py
+++ read_symptoms_lexicon.py
@@ -1,18 +1,17 @@
 import json
 import contractions
 
-f = open('depression_lexicon.json',r)
-data = json.load(f)
+def read_file():
+        f = open('depression_lexicon.json',r)
+        data = json.load(f)
 
-signal_1 = []
-for i in data['signal_1']:
-        i = i.replace("_", " ")
-        i = contractions.fix(i)
-        #print(i)
-        signal_1.append(i)
+        signal_1 = []
+        for i in data['signal_1']:
+                i = i.replace("_", " ")
+                i = contractions.fix(i)
+                #print(i)
+                signal_1.append(i)
 
-
-
-f.close()
-list_symptoms =[signal_1] 
-return list_symptpms
+        f.close()
+        list_symptoms =[signal_1] 
+        return list_symptpms
